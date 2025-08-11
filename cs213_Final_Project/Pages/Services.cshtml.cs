c7823cb4b28c2dd718872073d0493ce95b0eb550
ompug <Hariom.Pughat@outlook.com>
2025-08-11 14:36:34 -0400

    Created Services page and some minor changes

---
+++ cs213_Final_Project/Pages/Services.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cs213_Final_Project.Pages
+{
+    public class ServicesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
